1605161dc6b793a12a100911a9909b93e45d7b73
Michael Becker <alcexhim@gmail.com>
2016-06-16 00:42:17 -0400

    Keep calm and keep testing

--- Applications/UniversalWidgetToolkit.TestProject/MainWindow.cs
+++ Applications/UniversalWidgetToolkit.TestProject/MainWindow.cs
@@ -25,22 +25,26 @@ namespace UniversalWidgetToolkit.TestProject
 			layout.Orientation = Orientation.Vertical;
 			layout.Spacing = 13;
 
+			// this.MenuBar.Items.Add (new MenuItem ("Test"));
+
 			Label label = new Label();
+			label.Padding = new Padding (10);
+			label.HorizontalAlignment = HorizontalAlignment.Left;
 			label.Text = "System information goes here.";
 			this.Controls.Add(label);
 
 			Container containerButtons = new Container();
 			containerButtons.Layout = new BoxLayout();
 			(containerButtons.Layout as BoxLayout).Orientation = Orientation.Horizontal;
+			(containerButtons.Layout as BoxLayout).Alignment = Alignment.Far;
+			(containerButtons.Layout as BoxLayout).Spacing = 10;
 
-			Button button = new Button();
-			button.Text = "&OK";
-			button.Margin = new Padding(8);
+			Button button = new Button(StockButtonType.OK);
+			// button.Margin = new Padding(8);
 			button.Click += button_Click;
 
-			Button button2 = new Button();
+			Button button2 = new Button(StockButtonType.Cancel);
 			button2.Click += button2_Click;
-			button2.Text = "&Cancel";
 			button2.Margin = new Padding(8);
 
 			Button button3 = new Button();
@@ -73,7 +77,20 @@ namespace UniversalWidgetToolkit.TestProject
 
 		void button_Click(object sender, EventArgs e)
 		{
-			MessageDialog.ShowDialog("Test");
+			CommonDialogResult result = MessageDialog.ShowDialog("Do you want to frob the widgitator?", "Frob the Widgitator", MessageDialogButtons.YesNo, MessageDialogIcon.Question, MessageDialogModality.ApplicationModal, false, this);
+			switch (result)
+			{
+				case CommonDialogResult.Yes:
+				{
+					MessageDialog.ShowDialog ("Widgitator frobnicated successfully.", "Good News", MessageDialogButtons.OK, MessageDialogIcon.Information, MessageDialogModality.ApplicationModal, false, this);
+					break;
+				}
+				case CommonDialogResult.No:
+				{
+					MessageDialog.ShowDialog ("Widgitator not frobnicated. Try again later.", "As you wish", MessageDialogButtons.OK, MessageDialogIcon.Warning, MessageDialogModality.ApplicationModal, false, this);
+					break;
+				}
+			}
 		}
 
 		private void button2_Click(object sender, EventArgs e)

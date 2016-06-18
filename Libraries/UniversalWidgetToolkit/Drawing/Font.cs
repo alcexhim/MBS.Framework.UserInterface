56fae94edbbaf3a661328f476dcdb319de011003
Michael Becker <alcexhim@gmail.com>
2016-06-18 17:39:08 -0400

    Add Font common dialog

--- Libraries/UniversalWidgetToolkit/Drawing/Font.cs
+++ Libraries/UniversalWidgetToolkit/Drawing/Font.cs
@@ -10,6 +10,9 @@ namespace UniversalWidgetToolkit.Drawing
 		private string mvarFamilyName = String.Empty;
 		public string FamilyName { get { return mvarFamilyName; } set { mvarFamilyName = value; } }
 
+		private string mvarFaceName = String.Empty;
+		public string FaceName { get { return mvarFaceName; } set { mvarFaceName = value; } }
+
 		private double mvarSize = 0.0;
 		public double Size { get { return mvarSize; } set { mvarSize = value; } }
 
@@ -26,6 +29,18 @@ namespace UniversalWidgetToolkit.Drawing
 			font.Size = size;
 			font.Weight = weight;
 			return font;
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder sb = new StringBuilder ();
+			sb.Append (mvarFamilyName);
+
+			sb.Append (' ');
+			sb.Append (mvarSize.ToString ());
+			sb.Append (' ');
+			sb.Append (mvarFaceName);
+			return sb.ToString ();
 		}
 	}
 }

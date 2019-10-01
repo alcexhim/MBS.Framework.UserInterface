178378334923297fe0faf083b9f9261f2a9f25a0
Michael Becker <alcexhim@gmail.com>
2019-10-01 02:37:37 -0400

    boilerplate

--- Engines/WindowsForms/MBS.Framework.UserInterface.Engines.WindowsForms/Engines/WindowsForms/WindowsFormsNativeImplementation.cs
+++ Engines/WindowsForms/MBS.Framework.UserInterface.Engines.WindowsForms/Engines/WindowsForms/WindowsFormsNativeImplementation.cs
@@ -1,10 +1,61 @@
 ﻿using System;
+using System.Windows.Forms;
+using MBS.Framework.Drawing;
+using MBS.Framework.UserInterface.DragDrop;
+using MBS.Framework.UserInterface.Input.Keyboard;
+using MBS.Framework.UserInterface.Input.Mouse;
+
 namespace MBS.Framework.UserInterface.Engines.WindowsForms
 {
 	public abstract class WindowsFormsNativeImplementation : NativeImplementation
 	{
 		public WindowsFormsNativeImplementation (Engine engine, Control control) : base(engine, control)
 		{
+		}
+
+		protected override Dimension2D GetControlSizeInternal()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override Cursor GetCursorInternal()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override string GetTooltipTextInternal()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void RegisterDragSourceInternal(Control control, DragDropTarget[] targets, DragDropEffect actions, Input.Mouse.MouseButtons buttons, KeyboardModifierKey modifierKeys)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void RegisterDropTargetInternal(Control control, DragDropTarget[] targets, DragDropEffect actions, Input.Mouse.MouseButtons buttons, KeyboardModifierKey modifierKeys)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void SetControlVisibilityInternal(bool visible)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void SetCursorInternal(Cursor value)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void SetFocusInternal()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void SetTooltipTextInternal(string value)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }

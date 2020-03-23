7ed0fdfa3b39854f2b312f0dd99c8745b6d9b604
Michael Becker <alcexhim@gmail.com>
2020-03-23 05:11:18 -0400

    oops, forgot this

---
+++ Engines/WindowsForms/MBS.Framework.UserInterface.Engines.WindowsForms/Controls/Internal/CommandBars/ToolStripItemExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MBS.Framework.UserInterface.Engines.WindowsForms.Controls.Internal.CommandBars
+{
+	public static class ToolStripItemExtensions
+	{
+		public static ToolStripItem Clone(this ToolStripItem item)
+		{
+			ToolStripItem tsi = null;
+			if (item is ToolStripButton)
+			{
+				tsi = new ToolStripButton();
+			}
+			else if (item is ToolStripComboBox)
+			{
+				tsi = new ToolStripComboBox();
+			}
+			else if (item is ToolStripSplitButton)
+			{
+				tsi = new ToolStripSplitButton();
+				foreach (ToolStripItem tsmi in (item as ToolStripSplitButton).DropDownItems)
+				{
+					(tsi as ToolStripSplitButton).DropDownItems.Add(tsmi.Clone());
+				}
+			}
+			else if (item is ToolStripSeparator)
+			{
+				tsi = new ToolStripSeparator();
+			}
+			else if (item is ToolStripMenuItem)
+			{
+				tsi = new ToolStripMenuItem();
+				foreach (ToolStripItem tsmi in (item as ToolStripMenuItem).DropDownItems)
+				{
+					(tsi as ToolStripMenuItem).DropDownItems.Add(tsmi.Clone());
+				}
+			}
+			else if (item is ToolStripLabel)
+			{
+				tsi = new ToolStripLabel();
+			}
+			tsi.Name = item.Name;
+			tsi.Text = item.Text;
+			tsi.Image = item.Image;
+			tsi.DisplayStyle = item.DisplayStyle;
+
+			tsi.Tag = item;
+
+			tsi.Click += delegate (object sender, EventArgs e)
+			{
+				((ToolStripItem)((ToolStripItem)sender).Tag).PerformClick();
+			};
+			return tsi;
+		}
+	}
+}

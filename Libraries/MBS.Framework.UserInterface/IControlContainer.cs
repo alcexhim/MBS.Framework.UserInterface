ea29372813cfa44100ff422593265394acd6a96a
Michael Becker <alcexhim@gmail.com>
2020-04-10 14:12:28 -0400

    interface representing a (non-virtual?) Control container

---
+++ Libraries/MBS.Framework.UserInterface/IControlContainer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MBS.Framework.UserInterface
+{
+	public interface IControlContainer
+	{
+		Control[] GetAllControls();
+		Control.ControlCollection Controls { get; }
+	}
+}

0752f95843e40802278f644a4bc0cbdc230f722b
Michael Becker <alcexhim@gmail.com>
2020-08-13 02:26:28 -0400

    custom settings provider class for building your own settings-style dialogs

---
+++ Libraries/MBS.Framework.UserInterface/CustomSettingsProvider.cs
@@ -0,0 +1,49 @@
+﻿//
+//  CustomSettingsProvider.cs
+//
+//  Author:
+//       Michael Becker <alcexhim@gmail.com>
+//
+//  Copyright (c) 2020 Mike Becker's Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+namespace MBS.Framework.UserInterface
+{
+	public class CustomSettingsProvider : SettingsProvider
+	{
+		public event EventHandler SettingsLoaded;
+		protected virtual void OnSettingsLoaded(EventArgs e)
+		{
+			SettingsLoaded?.Invoke(this, e);
+		}
+		protected override void LoadSettingsInternal()
+		{
+			base.LoadSettingsInternal();
+			OnSettingsLoaded(EventArgs.Empty);
+		}
+
+		public event EventHandler SettingsSaved;
+		protected virtual void OnSettingsSaved(EventArgs e)
+		{
+			SettingsSaved?.Invoke(this, e);
+		}
+
+		protected override void SaveSettingsInternal()
+		{
+			base.SaveSettingsInternal();
+			OnSettingsSaved(EventArgs.Empty);
+		}
+	}
+}
